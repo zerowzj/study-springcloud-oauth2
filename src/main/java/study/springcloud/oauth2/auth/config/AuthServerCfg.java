a8a452e102c7a9e3bc0f1ea0391e8cee9f8de310
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-19 17:42:30 +0800

    修改代码

--- src/main/java/study/springcloud/oauth2/auth/config/AuthServerCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/AuthServerCfg.java
@@ -19,13 +19,12 @@ public class AuthServerCfg extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    /**
+     * 定义通过验证服务注册了哪些客户端应用程序
+     * ClientDetailsServiceConfigurer 支持两种不同类型的存储：内存存储和JDBC存储
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-
-        /**
-         * 定义通过验证服务注册了哪些客户端应用程序
-         * ClientDetailsServiceConfigurer 支持两种不同类型的存储：内存存储和JDBC存储
-         */
         clients.inMemory()
                 .withClient("security")
                 .secret("hui")
