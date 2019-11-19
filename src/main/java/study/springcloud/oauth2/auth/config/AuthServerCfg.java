4755458031f7b24299378973621c777b3944f694
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-19 12:57:31 +0800

    提交代码

--- src/main/java/study/springcloud/oauth2/auth/config/AuthServerCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/AuthServerCfg.java
@@ -1,47 +1,41 @@
 package study.springcloud.oauth2.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerCfg extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        String finalSecret = "{bcrypt}" + new BCryptPasswordEncoder().encode("123456");
 
-        // 配置两个客户端，一个用于password认证一个用于client认证
-        clients.inMemory().withClient("client_1")
-//                .resourceIds(Utils.RESOURCEIDS.ORDER)
-                .authorizedGrantTypes("client_credentials", "refresh_token")
-                .scopes("select")
-                .authorities("oauth2")
-                .secret(finalSecret)
-                .and().withClient("client_2")
-//                .resourceIds(Utils.RESOURCEIDS.ORDER)
-                .authorizedGrantTypes("password", "refresh_token")
-                .scopes("server")
-                .authorities("oauth2")
-                .secret(finalSecret);
+        /**
+         * 定义通过验证服务注册了哪些客户端应用程序
+         * ClientDetailsServiceConfigurer 支持两种不同类型的存储：内存存储和JDBC存储
+         */
+        clients.inMemory()
+                .withClient("security")
+                .secret("hui")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("select");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        endpoints.tokenStore(new MyRedisTokenStore(redisConnectionFactory))
-//                .authenticationManager(authenticationManager)
-//                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        // 允许表单认证
-        security.allowFormAuthenticationForClients();
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
