b4544bf4b63ab941f5d70a32c00868809d17f63c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-19 17:09:34 +0800

    更新代码

--- src/main/java/study/springcloud/oauth2/auth/config/WebSecurityCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/WebSecurityCfg.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
     @Bean
