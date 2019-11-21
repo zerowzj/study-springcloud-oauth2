7d959a3bf03134461efa6c5d0a6d49aa24db59ef
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-21 15:35:43 +0800

    更新代码

--- src/main/java/study/springcloud/oauth2/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/oauth2/support/SpringBootCfg.java
@@ -1,7 +1,6 @@
 package study.springcloud.oauth2.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 //@EnableEurekaClient
