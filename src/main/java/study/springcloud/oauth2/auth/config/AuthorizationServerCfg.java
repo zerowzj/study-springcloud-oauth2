daaf08df2ac52c3818b43ca327d5bffbd8c4a36b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-21 18:07:23 +0800

    更新代码

--- src/main/java/study/springcloud/oauth2/auth/config/AuthorizationServerCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/AuthorizationServerCfg.java
@@ -1,22 +1,15 @@
 package study.springcloud.oauth2.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.config.annotation.builders.ClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
-
-import java.util.Map;
 
 @Configuration
 public class AuthorizationServerCfg extends AuthorizationServerConfigurerAdapter {
@@ -40,7 +33,7 @@ public class AuthorizationServerCfg extends AuthorizationServerConfigurerAdapter
                 .withClient("client_id")
                 .secret("{noop}client_secret")
                 //支持的授权模式：授权码、密码、客户端凭证
-                .authorizedGrantTypes("authorization_code", "client_credentials", "refresh_token","password", "implicit")
+                .authorizedGrantTypes("authorization_code", "client_credentials", "refresh_token", "password", "implicit")
                 //定义访问作用域，也就是当用户使用某一个scope授权之后，可以根据不同的scope封装不同的user信息，
                 //比如webclient会封装角色，mobileclient封装角色和资源api，由开发人员定义即可
                 .scopes("webclient", "mobileclient");
