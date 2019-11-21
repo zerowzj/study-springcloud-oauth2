3ee9b1bb9b5ab829bc9ae4cb300c0f9a9bbf6a46
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-21 09:47:08 +0800

    更新代码

--- src/main/java/study/springcloud/oauth2/auth/config/AuthorizationServerCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/AuthorizationServerCfg.java
@@ -18,25 +18,27 @@ public class AuthorizationServerCfg extends AuthorizationServerConfigurerAdapter
     private UserDetailsService userDetailsService;
 
     /**
-     * 用来配置客户端详情服务，客户端详情信息在这里进行初始化，你能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息
-     * 定义通过验证服务注册了哪些客户端应用程序，ClientDetailsServiceConfigurer支持两种不同类型的存储：内存存储和JDBC存储
+     * 客户端配置
+     * 1. 客户端详情信息在这里进行初始化
+     * 2. 你能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息
+     * 3. 定义通过验证服务注册了哪些客户端应用程序，ClientDetailsServiceConfigurer支持两种不同类型的存储：内存存储和JDBC存储
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer client) throws Exception {
         client.inMemory()
                 .withClient("client_id")
                 .secret("client_secret")
-                //支持的授权模式 密码模式和客户端凭证
+                //支持的授权模式
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials")
                 //定义访问作用域，也就是当用户使用某一个scope授权之后，
-                // 可以根据不同的scope封装不同的user信息，
-                // 比如webclient会封装角色，mobileclient封装角色和资源api，由开发人员定义即可
+                //可以根据不同的scope封装不同的user信息，
+                //比如webclient会封装角色，mobileclient封装角色和资源api，由开发人员定义即可
                 .scopes("webclient", "mobileclient");
     }
 
     /**
-     * 用来配置授权（authorization）以及令牌（token）的访问端点和令牌服务(token services)
-     * 还有token的存储方式(tokenStore)
+     * 端点配置
+     * 1. 用来配置授权（authorization）以及令牌（token）的访问端点和令牌服务(token services)，还有token的存储方式(tokenStore)
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -46,7 +48,8 @@ public class AuthorizationServerCfg extends AuthorizationServerConfigurerAdapter
     }
 
     /**
-     * 用来配置令牌端点(Token Endpoint)的安全约束
+     * 111
+     * 1. 用来配置令牌端点(Token Endpoint)的安全约束
      */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
--- src/main/java/study/springcloud/oauth2/auth/config/WebSecurityCfg.java
+++ src/main/java/study/springcloud/oauth2/auth/config/WebSecurityCfg.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -45,12 +46,12 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
     /**
      * HttpSecurity中配置所有请求的安全验证
      */
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.requestMatchers().anyRequest()
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/oauth/**")
-//                .permitAll();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().anyRequest()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**")
+                .permitAll();
+    }
 }
